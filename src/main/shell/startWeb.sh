2459f07c86606271019f4210cec4e3b9566c4f19
stefan.marx <stefan.marx@appdynamics.com>
2014-06-11 19:45:32 +0200

    adding mmx console Args

--- src/main/shell/startWeb.sh
+++ src/main/shell/startWeb.sh
@@ -9,7 +9,12 @@ export JAVA_OPTIONS="-javaagent:$DIR/agent/javaagent.jar
 -Dappdynamics.controller.hostName=$controllerHost
 -Dappdynamics.controller.port=$controllerPort
 -Dappdynamics.agent.tierName=WebBackend
--Dappdynamics.agent.nodeName=Node4080"
+-Dappdynamics.agent.nodeName=Node4080
+-Dcom.sun.management.jmxremote
+-Dcom.sun.management.jmxremote.port=9991
+-Dcom.sun.management.jmxremote.local.only=false
+-Dcom.sun.management.jmxremote.authenticate=false
+-Dcom.sun.management.jmxremote.ssl=false"
 
 $DIR/web/node4080/bin/jetty.sh start
 
